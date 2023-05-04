bf8f8f10dbb551e7bd39b87e74cda61f3d4b3e27
Rodrigo Frumento <rodrigo.frumento@gmail.com>
2023-05-04 20:55:10 -0300

    start project

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-0aa2b7722dc1b5612"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+
+  tags = {
+    Name = "Instance Training"
+  }
+}
